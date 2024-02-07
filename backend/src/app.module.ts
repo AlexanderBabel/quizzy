fc552ca20351948e493b062aca81da4a726e33e9
“fsavgi” <fsavgi@gmail.com>
2024-02-07 16:37:37 +0100

    admın api app module changes

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { ConfigModule } from '@nestjs/config';
 import { AuthModule } from './auth/auth.module';
+import { AdminModule } from './admin/admin.module';
+
 
 @Module({
-  imports: [ConfigModule.forRoot(), AuthModule],
+  imports: [ConfigModule.forRoot(), AuthModule, AdminModule],
   controllers: [AppController],
   providers: [],
 })
