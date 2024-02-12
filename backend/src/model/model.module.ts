62d3276672bea2b3e6fd419c7e1a3144e3320498
Laswer5 <lukas.lindenthoming.9997@student.uu.se>
2024-02-12 15:10:58 +0100

    Resolved merge conflict changes

--- backend/src/model/model.module.ts
+++ backend/src/model/model.module.ts
@@ -2,11 +2,23 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { CreatorService } from './creator.service';
 import { PrismaModule } from 'nestjs-prisma';
+import { CacheModule } from '@nestjs/cache-manager'
+import * as redis from 'cache-manager-redis-store'
+import { CacheService } from './cache.service';
 
 @Module({
-  imports: [ConfigModule.forRoot(), PrismaModule],
+  imports: [ConfigModule.forRoot(), PrismaModule,
+    CacheModule.register(
+      {
+        isGlobal: true,
+        store: redis,
+        host: process.env.REDIS_HOST,
+        port: process.env.REDIS_PORT
+      }
+    )
+  ],
   controllers: [],
-  providers: [CreatorService],
-  exports: [CreatorService],
+  providers: [CreatorService, CacheService],
+  exports: [CreatorService, CacheService],
 })
 export class ModelModule {}
