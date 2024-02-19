5308c1e638125edf2a4ab3436bc971c2a54cf739
Alex Babel <alexander.babel@icloud.com>
2024-02-19 17:18:40 +0100

    chore: update migrations

--- backend/prisma/migrations/20240219161821_add_reports/migration.sql
+++ backend/prisma/migrations/20240219161821_add_reports/migration.sql
@@ -2,7 +2,7 @@
 CREATE TABLE "QuizReport" (
     "id" SERIAL NOT NULL,
     "quizId" INTEGER NOT NULL,
-    "creatorId" INTEGER NOT NULL,
+    "reporterId" INTEGER NOT NULL,
     "reason" TEXT NOT NULL,
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
 
@@ -13,4 +13,4 @@ CREATE TABLE "QuizReport" (
 ALTER TABLE "QuizReport" ADD CONSTRAINT "QuizReport_quizId_fkey" FOREIGN KEY ("quizId") REFERENCES "Quiz"("id") ON DELETE CASCADE ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "QuizReport" ADD CONSTRAINT "QuizReport_creatorId_fkey" FOREIGN KEY ("creatorId") REFERENCES "Creator"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE "QuizReport" ADD CONSTRAINT "QuizReport_reporterId_fkey" FOREIGN KEY ("reporterId") REFERENCES "Creator"("id") ON DELETE CASCADE ON UPDATE CASCADE;
