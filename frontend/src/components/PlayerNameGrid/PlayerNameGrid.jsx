18660b787d2189bc676e2d3c8a420bb77dc49ab0
natta p <natashazaza@live.se>
2024-02-29 16:52:43 +0100

    lobby css

--- frontend/src/components/PlayerNameGrid/PlayerNameGrid.jsx
+++ frontend/src/components/PlayerNameGrid/PlayerNameGrid.jsx
@@ -3,13 +3,11 @@ import "./PlayerNameGrid.css";
 export default function PlayerNameGrid({ players }) {
   return (
     <div id="PlayerNameGrid">
-      <div className="NameContainer">
         {players.map(({ id, name }) => (
-          <p key={id} className="playerName">
+          <div key={id} className="playerName">
             {name}
-          </p>
+          </div>
         ))}
-      </div>
     </div>
   );
 }
--- frontend/src/pages/LobbyPage/LobbyPage.jsx
+++ frontend/src/pages/LobbyPage/LobbyPage.jsx
@@ -1,16 +1,16 @@
-import "./LobbyPage.css";
-import background from "./../../images/blob-scene-haikei-2.svg";
-import StartGameBtn from "../../components/Buttons/StartGameBtn";
-import PlayerCounter from "../../components/PlayerCounter/PlayerCounter";
-import { useEffect } from "react";
-import { useLocation, useNavigate, useParams } from "react-router-dom";
+import './LobbyPage.css';
+import background from './../../images/blob-scene-haikei-2.svg';
+import StartGameBtn from '../../components/Buttons/StartGameBtn';
+import PlayerCounter from '../../components/PlayerCounter/PlayerCounter';
+import { useEffect } from 'react';
+import { useLocation, useNavigate, useParams } from 'react-router-dom';
 
-import { useSocketEvent } from "socket.io-react-hook";
-import useAuthenticatedSocket from "../../context/useAuthenticatedSocket";
-import useLobby, { GameRole, LobbyActionType } from "../../context/useLobby";
-import PlayerNameGrid from "../../components/PlayerNameGrid/PlayerNameGrid";
-import UsernameTextField from "../../components/PlayerNameInput/PlayerNameInput";
-import { enqueueSnackbar } from "notistack";
+import { useSocketEvent } from 'socket.io-react-hook';
+import useAuthenticatedSocket from '../../context/useAuthenticatedSocket';
+import useLobby, { GameRole, LobbyActionType } from '../../context/useLobby';
+import PlayerNameGrid from '../../components/PlayerNameGrid/PlayerNameGrid';
+import UsernameTextField from '../../components/PlayerNameInput/PlayerNameInput';
+import { enqueueSnackbar } from 'notistack';
 
 export default function LobbyPage() {
   const navigate = useNavigate();
@@ -20,18 +20,18 @@ export default function LobbyPage() {
   const { lobbyCode } = useParams();
 
   const { lastMessage: joinLobbyResponse, sendMessage: joinLobby } =
-    useSocketEvent(socket, "lobby:join");
+    useSocketEvent(socket, 'lobby:join');
   const { lastMessage: createResponse, sendMessage: createLobby } =
-    useSocketEvent(socket, "lobby:create");
+    useSocketEvent(socket, 'lobby:create');
   const { lastMessage: startGameResponse, sendMessage: startQuiz } =
-    useSocketEvent(socket, "lobby:start");
+    useSocketEvent(socket, 'lobby:start');
 
   // create lobby is quizId is provided
   useEffect(() => {
     if (!socket.connected) return;
 
     if (quizId && state.lobbyCode === null) {
-      console.log("Creating lobby", quizId);
+      console.log('Creating lobby', quizId);
       createLobby(quizId);
     }
   }, [socket.connected]); // eslint-disable-line react-hooks/exhaustive-deps
@@ -51,19 +51,19 @@ export default function LobbyPage() {
   // handle quiz start response
   useEffect(() => {
     if (startGameResponse) {
-      navigate("/game");
-      enqueueSnackbar("Game started!", { variant: "success" });
+      navigate('/game');
+      enqueueSnackbar('Game started!', { variant: 'success' });
       return;
     }
   }, [startGameResponse]); // eslint-disable-line react-hooks/exhaustive-deps
 
   // handle join lobby response
   useEffect(() => {
-    if (joinLobbyResponse?.state === "game") {
-      navigate("/game");
+    if (joinLobbyResponse?.state === 'game') {
+      navigate('/game');
       enqueueSnackbar(
-        "Game is already running. Continuing with next question...",
-        { variant: "info" }
+        'Game is already running. Continuing with next question...',
+        { variant: 'info' }
       );
       dispatch({
         type: LobbyActionType.JOIN_LOBBY,
@@ -72,9 +72,9 @@ export default function LobbyPage() {
       });
     }
 
-    if (joinLobbyResponse?.state === "lobby") {
-      enqueueSnackbar("Lobby joined! Waiting for start...", {
-        variant: "success",
+    if (joinLobbyResponse?.state === 'lobby') {
+      enqueueSnackbar('Lobby joined! Waiting for start...', {
+        variant: 'success',
       });
       dispatch({
         type: LobbyActionType.JOIN_LOBBY,
@@ -86,54 +86,66 @@ export default function LobbyPage() {
 
   const svgStyle = {
     backgroundImage: `url(${background})`,
-    backgroundRepeat: "no-repeat",
-    backgroundSize: "cover",
-    width: "100vw",
-    height: "100vh",
+    backgroundRepeat: 'no-repeat',
+    backgroundSize: 'cover',
+    width: '100vw',
+    height: '100vh',
     margin: 0,
     padding: 0,
-    overflow: "hidden",
+    overflow: 'hidden',
   };
 
-  const quizName = "Quiz Lobby";
+  const quizName = 'This is the title of the quiz ';
 
   return (
-    <div className="lobbyPage" style={svgStyle}>
-      <div>
-        <div className="header">
-          <h1 className="lobbyTitle">{quizName}</h1>
-          {state.role === GameRole.HOST && state.players?.length > 0 && (
-            <StartGameBtn text="Start Game" onClick={() => startQuiz()} />
-          )}
+    <div className='lobbyPage' style={svgStyle}>
+      <div className='lobbyPageTop'>
+        <h1 className='lobbyTitle'>{quizName}</h1>
+        <div className='lobbyBtns'>
           {state.role !== null && (
             <PlayerCounter playerCount={state.players.length}></PlayerCounter>
           )}
-        </div>
-        <h1 className="lobbyCodeTitle">
-          Game Pin: {state.lobbyCode ?? lobbyCode}
-        </h1>
-        <p className="lobbyCodeTitle">
-          Go to {window.location.origin}/join/{state.lobbyCode ?? lobbyCode} to
-          join this lobby.
-        </p>
-      </div>
-      {state.role !== null ? (
-        <div>
-          {state.role === GameRole.HOST &&
-            (state.players.length === 0 ? (
-              <h1 style={{ color: "white" }}>Waiting for players...</h1>
-            ) : (
-              <PlayerNameGrid players={state.players}></PlayerNameGrid>
-            ))}
-          {state.role === GameRole.PLAYER && (
-            <h1 style={{ color: "white" }}>Waiting for game to start...</h1>
+          {state.role === GameRole.HOST && state.players?.length > 0 && (
+            <StartGameBtn text='Start Game' onClick={() => startQuiz()} />
           )}
         </div>
+        <h1 className='lobbyCodeTitle'>
+          Game Pin: {state.lobbyCode ?? lobbyCode}
+        </h1>
+      </div>
+
+      {state.role !== null ? (
+        <>
+          {state.role === GameRole.HOST &&
+            (state.players.length === 0 ? (
+              <div className='lobbyPageBottom'>
+                <h1>Waiting for players...</h1>
+              </div>
+            ) : (
+              <div className='playerNameGridDiv'>
+                <PlayerNameGrid players={state.players}></PlayerNameGrid>
+              </div>
+            ))}
+          {state.role === GameRole.PLAYER && (
+            <>
+              <div className='playerNameGridDiv'>
+                {state.players.length > 0 && (
+                  <PlayerNameGrid players={state.players}></PlayerNameGrid>
+                )}
+              </div>
+              <div className='lobbyPageBottom'>
+                <h1>Waiting for game to start...</h1>
+              </div>
+            </>
+          )}
+        </>
       ) : (
-        <UsernameTextField
-          className="playerNameInput"
-          onSubmitted={(userName) => joinLobby({ lobbyCode, userName })}
-        ></UsernameTextField>
+        <div className='lobbyPageBottom'>
+          <UsernameTextField
+            className='playerNameInput'
+            onSubmitted={(userName) => joinLobby({ lobbyCode, userName })}
+          ></UsernameTextField>
+        </div>
       )}
     </div>
   );
