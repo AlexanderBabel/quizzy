133f0300f6da4765a800298e02022cd04f754785
Alex Babel <alexander.babel@icloud.com>
2024-02-29 01:12:25 +0100

    chore: update formatting

--- frontend/src/components/ChosenAnswers/ChosenAnswers.jsx
+++ frontend/src/components/ChosenAnswers/ChosenAnswers.jsx
@@ -1,37 +1,27 @@
-import './ChosenAnswers.css';
-import swine from '../../images/Swine.png';
-import mouse from '../../images/Mouse.png';
-import frog from '../../images/Frog.png';
-import sheep from '../../images/Sheep.png';
-
-const ChosenAnswer = ({ count, backgroundColor }) => {
-  return (
-    <div
-      className='countBar'
-      style={{ height: `${count * 2}rem`, background: backgroundColor }}
-    >
-      {count}
-    </div>
-  );
-};
+import "./ChosenAnswers.css";
+import swine from "../../images/Swine.png";
+import mouse from "../../images/Mouse.png";
+import frog from "../../images/Frog.png";
+import sheep from "../../images/Sheep.png";
+import ChosenAnswer from "../ChosenAnswer/ChosenAnswer";
 
 export default function ChosenAnswers() {
   const counts = [4, 5, 6, 2];
   const icons = [frog, sheep, mouse, swine];
 
-  const barColors = ['#8ABBFE', '#FFFF71', '#FB9D4A', '#56E75B'];
+  const barColors = ["#8ABBFE", "#FFFF71", "#FB9D4A", "#56E75B"];
 
   return (
-    <div className='chosenAnswers'>
+    <div className="chosenAnswers">
       <div>
-        <div className='bars'>
+        <div className="bars">
           {counts.map((count, i) => (
             <ChosenAnswer count={count} backgroundColor={barColors[i]} />
           ))}
         </div>
-        <div className='icons'>
+        <div className="icons">
           {icons.map((icon) => (
-            <img src={icon} alt='icon' className='answerIcon' />
+            <img src={icon} alt="icon" className="answerIcon" />
           ))}
         </div>
       </div>
--- frontend/src/components/PlayerScore/PlayerScore.jsx
+++ frontend/src/components/PlayerScore/PlayerScore.jsx
@@ -1,8 +1,8 @@
-import './PlayerScore.css';
+import "./PlayerScore.css";
 
 export default function PlayerScore({ playerName, playerScore }) {
   return (
-    <div className='playerScoreWrapper'>
+    <div className="playerScoreWrapper">
       <div>{playerName}</div>
       <div>{playerScore}</div>
     </div>
