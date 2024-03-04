c5c9d4b86d6b1a70f830fb44df4d911062fa34d6
Laswer5 <lukas.lindenthoming.9997@student.uu.se>
2024-03-04 01:42:18 +0100

    Added ReponseQuiz formatting

--- backend/src/quiz/quiz.controller.ts
+++ backend/src/quiz/quiz.controller.ts
@@ -32,14 +32,16 @@ export class QuizController {
 
   @IsPublic()
   @Get('/')
-  async last20Quizzes(): Promise<Quiz[]> {
-    return this.quizModelService.findQuizzes({
+  async last20Quizzes(): Promise<ResponseQuiz[]> {
+    const quizzes = await this.quizModelService.findQuizzes({
       take: 20,
       orderBy: {
         createdAt: 'desc',
       },
       where: { visibility: QuizVisibility.PUBLIC },
     });
+
+    return quizzes.map((quiz) => this.quizService.formatQuiz(quiz));
   }
 
   @IsPublic()
