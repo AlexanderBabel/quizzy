3f0e4f8d2735f44b2353be09c1a6d83e4b05ebae
natta p <natashazaza@live.se>
2024-03-13 16:46:57 +0100

    css

--- frontend/src/components/ChosenAnswer/ChosenAnswer.jsx
+++ frontend/src/components/ChosenAnswer/ChosenAnswer.jsx
@@ -4,7 +4,7 @@ export default function ChosenAnswer({ count, backgroundColor }) {
   return (
     <div
       className="countBar"
-      style={{ height: `${count * 2}rem`, background: backgroundColor }}
+      style={{ height: `${count * 2}rem`, background: backgroundColor , maxHeight:'30rem'}}
     >
       {count > 0 && count}
     </div>
